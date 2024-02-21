ac82db7463d1c32e584669e72bf01450215f86a0
Zachary Kohs <zachary.c.kohs@gmail.com>
2024-02-21 13:00:07 -0500

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Threading;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Welcome to the Delorean Time Machine");
+        Console.WriteLine("Please enter your credentials");
+        Console.Write("username: ");
+        string userName = Console.ReadLine();
+        Console.WriteLine(userName);
+
+        /*
+        for (int i = 0; i <= 10; i++)
+        {
+            Console.Clear(); // Clear the console
+            Console.WriteLine($"Counter: {i}"); // Display new content
+            Thread.Sleep(1000); // Wait for a second
+        }
+        */
+    }
+}
