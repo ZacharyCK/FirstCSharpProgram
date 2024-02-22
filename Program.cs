974682aabf333add5661aaef2ce6b1e565bc45b7
Zachary Kohs <zachary.c.kohs@gmail.com>
2024-02-22 15:19:17 -0500

    Added looping for when user wants to travel to a different year. Added while loop that makes it so you don't exit program until you type exit.

--- Program.cs
+++ Program.cs
@@ -12,71 +12,112 @@ class Program
         User Doc = new User("Doc_Brown", "great_scott");
         User[] users = { Marty, Doc };
 
-        Console.WriteLine("Welcome to the Delorean Time Machine");
-        Console.WriteLine("Please enter your credentials");
-        Console.Write("username: ");
-        string userName = Console.ReadLine();
-        Console.Write("password: ");
-        string password = Console.ReadLine();
-        for(int i = 0; i < users.Length; i++)
+        bool carRunning = true;
+
+        while(carRunning)
         {
-            if (users[i].UserName == userName && users[i].Password == password) 
+            Console.Clear(); // Clear the console
+            Console.WriteLine("Welcome to the Delorean Time Machine");
+            Console.WriteLine("Please enter your credentials");
+            Console.Write("username: ");
+            string userName = Console.ReadLine();
+            Console.Write("password: ");
+            string password = Console.ReadLine();
+            for (int i = 0; i < users.Length; i++)
             {
-                Console.WriteLine($"Welcome, {userName}");
-                Console.Write($"What year would you like to visit today? ");
-                string year = Console.ReadLine();
-                string yearPattern = @"^\d{4}$";
-                Match yearMatch = Regex.Match(year, yearPattern);
-                if (yearMatch.Success)
+                if (users[i].UserName == userName && users[i].Password == password)
                 {
-                    Console.WriteLine("Buckle Up!");
-                    for (int j = 0; j <= 5; j++)
+                    bool goingToDrive = true;
+                    bool initialStartUp = true;
+                    while (goingToDrive)
                     {
-                        Console.Clear(); // Clear the console
-                        Console.WriteLine($"Speedometer: {j} mph"); // Display new content
-                        Thread.Sleep(150); // Wait for .15 seconds
+                        if (initialStartUp)
+                        {
+                            Console.WriteLine($"Welcome, {userName}");
+                            Console.Write($"What year would you like to visit today? ");
+                        }
+                        else
+                        {
+                            Console.Write($"What year would you like to visit? ");
+                        }
+                        string year = Console.ReadLine();
+                        string yearPattern = @"^\d{4}$";
+                        Match yearMatch = Regex.Match(year, yearPattern);
+                        if (yearMatch.Success)
+                        {
+                            Console.WriteLine("Buckle Up!");
+                            for (int j = 0; j <= 5; j++)
+                            {
+                                Console.Clear(); // Clear the console
+                                Console.WriteLine($"Speedometer: {j} mph"); // Display new content
+                                Thread.Sleep(150); // Wait for .15 seconds
+                            }
+                            for (int k = 6; k <= 15; k++)
+                            {
+                                Console.Clear(); // Clear the console
+                                Console.WriteLine($"Speedometer: {k} mph"); // Display new content
+                                Thread.Sleep(100); // Wait for .1 seconds
+                            }
+                            for (int l = 16; l <= 30; l++)
+                            {
+                                Console.Clear(); // Clear the console
+                                Console.WriteLine($"Speedometer: {l} mph"); // Display new content
+                                Thread.Sleep(50); // Wait for .05 seconds
+                            }
+                            for (int m = 31; m <= 50; m++)
+                            {
+                                Console.Clear(); // Cmear the consome
+                                Console.WriteLine($"Speedometer: {m} mph"); // Dispmay new content
+                                Thread.Sleep(20); // Wait for .020 seconds
+                            }
+                            for (int n = 51; n <= 88; n++)
+                            {
+                                Console.Clear(); // Cmear the consome
+                                Console.WriteLine($"Speedometer: {n} mph"); // Dispmay new content
+                                Thread.Sleep(15); // Wait for .015 seconds
+                            }
+                            string woosh = "WWWWWWOOOOOOOOOOOOOOOOOSSSSSSSSHHHHHHHHHHHHH!!!!!!!!";
+                            Thread.Sleep(600);
+                            for (int o = 0; o < woosh.Length; o++)
+                            {
+                                Console.Write(woosh[o]);
+                                Thread.Sleep(40);
+                            }
+                            Console.WriteLine("");
+                            Thread.Sleep(600);
+                            Console.WriteLine($"Welcome to the year {year}, {userName}");
+                            Console.Write("Would you like to visit another year? (Y/N) ");
+                            string visitAnotherYear = Console.ReadLine();
+                            if (visitAnotherYear == "Y")
+                            {
+                                initialStartUp = false;
+                                continue;
+                            }
+                            else
+                            {
+                                goingToDrive = false;
+                            }
+                            Console.Write("Would you like to start over (LogOff) or exit vehicle (exit)? ");    
+                            string keepCarRunning = Console.ReadLine();
+                            if (keepCarRunning == "LogOff") 
+                            {
+                                break;
+                            }
+                            else
+                            {
+                                carRunning = false;
+                                break;
+                            }
+                        }
+                        else
+                        {
+                            Console.WriteLine("That's not a year!");
+                        }
                     }
-                    for (int k = 6; k <= 15; k++)
-                    {
-                        Console.Clear(); // Clear the console
-                        Console.WriteLine($"Speedometer: {k} mph"); // Display new content
-                        Thread.Sleep(100); // Wait for .1 seconds
-                    }
-                    for (int l = 16; l <= 30; l++)
-                    {
-                        Console.Clear(); // Clear the console
-                        Console.WriteLine($"Speedometer: {l} mph"); // Display new content
-                        Thread.Sleep(50); // Wait for .05 seconds
-                    }
-                    for (int m = 31; m <= 50; m++)
-                    {
-                        Console.Clear(); // Cmear the consome
-                        Console.WriteLine($"Speedometer: {m} mph"); // Dispmay new content
-                        Thread.Sleep(20); // Wait for .020 seconds
-                    }
-                    for (int n = 51; n <= 88; n++)
-                    {
-                        Console.Clear(); // Cmear the consome
-                        Console.WriteLine($"Speedometer: {n} mph"); // Dispmay new content
-                        Thread.Sleep(15); // Wait for .015 seconds
-                    }
-                    string woosh = "WWWWWWOOOOOOOOOOOOOOOOOSSSSSSSSHHHHHHHHHHHHH!!!!!!!!";
-                    Thread.Sleep(600);
-                    for (int o = 0; o < woosh.Length; o++)
-                    {
-                            Console.Write(woosh[o]);
-                            Thread.Sleep(40);
-                    }
-                    Console.WriteLine("");
-                    Thread.Sleep(600);
-                    Console.WriteLine($"Welcome to the year {year}, {userName}");
-                }
-                else
-                {
-                    Console.WriteLine("That's not a year!");
                 }
             }
         }
+        
 
         /*
         for (int i = 0; i <= 10; i++)
